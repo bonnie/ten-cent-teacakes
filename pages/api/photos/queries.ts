bce1cfb547f978a3ef4ca4eca0e42b7d1275efb3
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-10-05 16:57:49 -0700

    Add photographer to photo info

--- pages/api/photos/queries.ts
+++ pages/api/photos/queries.ts
@@ -2,16 +2,25 @@
 import prisma from "../../../lib/prisma";
 import { Prisma } from ".prisma/client";
 
-type PhotoPutData = { eventId: number; imagePath: string };
+type PhotoPutData = {
+  eventId: number;
+  imagePath: string;
+  photographer: string;
+};
 
 export const getPhotosSortDescending = () =>
   prisma.photo.findMany({
     orderBy: { createdAt: "desc" },
   });
 
-export const addPhoto = ({ imagePath, eventId }: PhotoPutData) => {
+export const addPhoto = ({
+  imagePath,
+  eventId,
+  photographer,
+}: PhotoPutData) => {
   const data: Prisma.PhotoCreateInput = {
     imagePath,
+    photographer,
     event: { connect: { id: eventId } },
   };
 
