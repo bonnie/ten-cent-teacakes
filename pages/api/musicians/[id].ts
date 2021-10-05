75070508527826178b34496cab62e93d763e980d
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-10-05 16:45:08 -0700

    Musician api and prisma queries

--- pages/api/musicians/[id].ts
+++ pages/api/musicians/[id].ts
@@ -1,24 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { processApiError } from "../../../lib/api";
-import { addMusician, getMusiciansSortAscending } from "./queries";
+import { deleteMusician, getMusicianById } from "./queries";
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { body, method } = req;
+  const { method, query } = req;
+  const { id: idString } = query;
+  const id = Number(idString);
 
   try {
     switch (method) {
       case "GET":
-        res.json(await getMusiciansSortAscending());
+        res.status(200).json(await getMusicianById(id));
         break;
-      case "PUT":
-        res.status(201).json(await addMusician(body));
+      case "DELETE":
+        res.status(204).json(await deleteMusician(id));
         break;
       default:
-        res.setHeader("Allow", ["GET", "PUT"]);
+        res.setHeader("Allow", ["GET", "DELETE"]);
         res.status(405).end(`Method ${method} Not Allowed`);
     }
   } catch (error) {
--- pages/api/musicians/queries.ts
+++ pages/api/musicians/queries.ts
@@ -2,25 +2,65 @@
 import prisma from "../../../lib/prisma";
 import { Prisma } from ".prisma/client";
 
-type PhotoPutData = { eventId: number; imagePath: string };
-
-export const getPhotosSortDescending = () =>
-  prisma.photo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
-
-export const addPhoto = ({ imagePath, eventId }: PhotoPutData) => {
-  const data: Prisma.PhotoCreateInput = {
-    imagePath,
-    event: { connect: { id: eventId } },
-  };
-
-  return prisma.photo.create({ data });
+type InstrumentData = {
+  name: string;
 };
 
-export const getPhotoById = (id: number) =>
-  prisma.photo.findUnique({ where: { id } });
+type NewOrExistingInstrument = {
+  instrumentId?: number;
+  instrumentData?: InstrumentData;
+};
 
-export const deletePhoto = async (id: number) => {
-  prisma.photo.delete({ where: { id } });
+type MusicianPutData = {
+  firstName: string;
+  lastName: string;
+  bio: string;
+  imagePath: string;
+  instruments: Array<NewOrExistingInstrument>;
+};
+
+const generateInstrumentData = (
+  instruments: Array<NewOrExistingInstrument>,
+) => {
+  // connect to instrument if id was provided; otherwise create a new one
+  const connectInstruments = instruments
+    .filter((instrument) => instrument.instrumentId)
+    .map((instrument) => ({ id: instrument.instrumentId }));
+  const createInstruments = instruments
+    .filter((instrument) => instrument.instrumentData)
+    .map((instrument) => {
+      // filter should take care of this; simply satisfying typescript
+      if (!instrument.instrumentData)
+        throw new Error("trying to create instrument without data");
+      return instrument.instrumentData;
+    });
+  return { create: createInstruments, connect: connectInstruments };
+};
+
+export const getMusiciansSortAscending = () =>
+  prisma.musician.findMany({
+    orderBy: { lastName: "asc" },
+  });
+
+export const addMusician = (rawData: MusicianPutData) => {
+  const { firstName, lastName, bio, imagePath, instruments } = rawData;
+  const instrumentsData = generateInstrumentData(instruments);
+
+  const data: Prisma.MusicianCreateInput = {
+    firstName,
+    lastName,
+    bio,
+    imagePath,
+    instruments: instrumentsData,
+  };
+
+  return prisma.musician.create({ data });
+};
+
+export const getMusicianById = async (id: number) => {
+  prisma.musician.findUnique({ where: { id } });
+};
+
+export const deleteMusician = async (id: number) => {
+  prisma.musician.delete({ where: { id } });
 };
