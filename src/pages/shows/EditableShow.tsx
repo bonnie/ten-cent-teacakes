95c303541f1980c46f1ac06bf05df11ffafb3185
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-10-22 11:32:18 -0700

    WIP: start to implement react-hook-form

--- src/pages/shows/EditableShow.tsx
+++ src/pages/shows/EditableShow.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import { EditButtons } from "@/components/lib/EditButtons";
 import { ShowWithVenue } from "@/lib/api";
+import { ShowPatchData, ShowPutData } from "@/pages/api/shows/queries";
 
-import { ShowPutData } from "../api/shows/queries";
 import { formattedPerformAt } from "./utils";
 import { Venue } from "./Venue";
 
@@ -17,11 +19,17 @@ export const EditableNewShow: React.FC<EditableNewShowProps> = ({
   const newShow: ShowPutData = {
     performAt: new Date(),
   };
+  const cancelAddingShow = () => setAddingShow(false);
+  const handleSave = (data: ShowPutData) => {
+    // TODO: server call to save new show
+  };
   return (
     <EditableShow
       show={newShow}
       startWithEditing
-      setAddingShow={setAddingShow}
+      handleCancel={cancelAddingShow}
+      handleDelete={cancelAddingShow}
+      handleSave={handleSave}
     />
   );
 };
@@ -32,57 +40,72 @@ type EditableExistingShowProps = {
 
 export const EditableExistingShow: React.FC<EditableExistingShowProps> = ({
   show,
-}) => (
-  <EditableShow
-    show={show}
-    startWithEditing={false}
-    setAddingShow={undefined}
-  />
-);
+}) => {
+  const handleDelete = () => {
+    // TODO server call to delete show
+  };
+  const handleSave = (data: ShowPatchData) => {
+    // TODO: server call to save show
+  };
+  return (
+    <EditableShow
+      show={show}
+      startWithEditing={false}
+      handleCancel={undefined}
+      handleDelete={handleDelete}
+      handleSave={handleSave}
+    />
+  );
+};
 
 type EditShowProps = {
   show: ShowWithVenue | ShowPutData;
   startWithEditing: boolean;
-  setAddingShow: React.Dispatch<React.SetStateAction<boolean>> | undefined;
+  handleCancel: (() => void) | undefined;
+  handleSave: ((data: ShowPatchData) => void) | ((data: ShowPutData) => void);
+  handleDelete: () => void;
 };
 
 const EditableShow: React.FC<EditShowProps> = ({
   show,
   startWithEditing,
-  setAddingShow,
+  handleCancel,
+  handleSave,
+  handleDelete,
 }) => {
   const [editing, setEditing] = useState(startWithEditing);
-  const existingShow = "id" in show;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  const handleCancel = () => {
-    if (existingShow) {
-      setEditing(false);
-    } else if (setAddingShow) setAddingShow(false);
-  };
-  const handleSubmit = () => {
-    if (existingShow) {
-      // TODO: update show
-    } else {
-      // TODO: add new show
-    }
-  };
-  const handleDelete = () => {
-    if (existingShow) {
-      // TODO: send server call to delete show
-    } else if (setAddingShow) setAddingShow(false);
+  const stopEditing = () => {
+    setEditing(false);
   };
   const venue = "venue" in show ? show.venue : undefined;
 
   return (
     <div>
       <p>
-        <EditButtons
-          onCancel={handleCancel}
-          onSave={handleSubmit}
-          onDelete={handleDelete}
-        />
-        {formattedPerformAt(show.performAt)}
-        <Venue venue={venue} editing={editing} />
+        <form onSubmit={handleSubmit(handleSave)}>
+          <EditButtons
+            onCancel={handleCancel ?? stopEditing}
+            onSave={() => handleSubmit(handleSave)}
+            onDelete={handleDelete}
+          />
+          {editing ? (
+            <input
+              type="datetime-local"
+              defaultValue={show.performAt.toLocaleDateString()}
+              {...(register("performAt"), { required: true })}
+            />
+          ) : (
+            formattedPerformAt(show.performAt)
+          )}
+          <Venue venue={venue} editing={editing} />
+        </form>
       </p>
     </div>
   );
