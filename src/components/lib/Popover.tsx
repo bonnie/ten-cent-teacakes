eaabf360d34469e3f93b144d77794618addc79db
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-01 18:44:14 -0800

    Add popover file

---
+++ src/components/lib/Popover.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { tw } from "twind";
+
+export const Popover: React.FC<{
+  message: string;
+}> = ({ message, children }) => {
+  const [popoverHidden, setPopoverHidden] = useState(true);
+  const showPopover = () => setPopoverHidden(false);
+  const hidePopover = () => setPopoverHidden(true);
+
+  const classes = tw([
+    popoverHidden ? "hidden" : null,
+    "px-6",
+    "py-2.5",
+    "bg-white",
+    "text-aqua-800",
+    "font-medium",
+    "leading-tight",
+    "rounded",
+    "shadow-lg",
+    "focus:outline-none",
+    "focus:ring-0",
+    "transition",
+    "duration-150",
+    "ease-in-out",
+    "overflow-x-hidden",
+    "overflow-y-auto",
+    "absolute",
+    "z-50",
+    "outline-none",
+    "focus:outline-none",
+    "text-lg",
+  ]);
+
+  return (
+    <>
+      <span
+        onMouseOver={showPopover}
+        onFocus={showPopover}
+        onMouseOut={hidePopover}
+        onBlur={hidePopover}
+      >
+        {children}
+      </span>
+      <button type="button" className={classes} onClick={hidePopover}>
+        {message}
+      </button>
+    </>
+  );
+};
