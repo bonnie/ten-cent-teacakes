b39c2c631015838dfb5a6bc35d6765185bce5de1
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-07 12:39:32 -0800

    add photo image display and flex

--- src/pages/photos/components/Photos.tsx
+++ src/pages/photos/components/Photos.tsx
@@ -1,12 +1,17 @@
 import { Photo as PhotoType } from "@prisma/client";
 
 import React from "react";
+import { tw } from "twind";
 
 import { usePhotos } from "../hooks/usePhotos";
 
 const Photo: React.FC<{ photo: PhotoType }> = ({ photo }) => (
-  <div>
-    <img src={photo.imagePath} alt="Ten-cent Teacakes" />
+  <div className={tw(["m-5"])}>
+    <img
+      className={tw(["max-w-48 max-h-48 border-solid border-4 border-black"])}
+      src={photo.imagePath}
+      alt="Ten-cent Teacakes"
+    />
   </div>
 );
 
@@ -14,7 +19,7 @@ export const Photos: React.FC = () => {
   const { photos } = usePhotos();
 
   return (
-    <div className="flex">
+    <div className="flex flex-wrap justify-center items-center">
       {photos.map((photo) => (
         <Photo key={photo.id} photo={photo} />
       ))}
