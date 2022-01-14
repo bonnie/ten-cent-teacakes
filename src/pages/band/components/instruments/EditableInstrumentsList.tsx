080884e23af5a8f2e494e96b9a34607019da9456
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-14 14:17:00 -0800

    Multi-select with drop-down

--- src/pages/band/components/instruments/EditableInstrumentsList.tsx
+++ src/pages/band/components/instruments/EditableInstrumentsList.tsx
@@ -1,9 +1,23 @@
-import { useField } from "formik";
+import { Instrument } from "@prisma/client";
+
 import React from "react";
+
+import { MultiSelect } from "@/components/lib/form/MultiSelectInput";
 
 import { useInstruments } from "../../hooks/useInstruments";
 
 export const EditableInstrumentsList: React.FC = () => {
   const { instruments } = useInstruments();
-  return <div />;
+  const instrumentOptions = instruments.map((instrument) => ({
+    value: instrument.id,
+    display: instrument.name,
+  }));
+  return (
+    <MultiSelect
+      name="instrumentId"
+      required
+      label="Select instruments"
+      options={instrumentOptions}
+    />
+  );
 };
