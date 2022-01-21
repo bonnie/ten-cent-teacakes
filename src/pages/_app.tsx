0c9a6bb00e3c1a34dcb6c58637b4fcc4baa83510
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-21 14:28:45 -0800

    Add sentry error handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import "tailwindcss/tailwind.css";
 import "@/styles/globals.css";
 
 import { UserProvider } from "@auth0/nextjs-auth0";
+import * as Sentry from "@sentry/nextjs";
 import { AppProps } from "next/app";
 import React from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 import { Layout } from "@/components/Layout";
+import { ErrorComponent } from "@/components/lib/ErrorComponent";
 import { ToastContainer } from "@/components/toasts/ToastContainer";
 import { ToastProvider } from "@/components/toasts/ToastContext";
 
@@ -15,17 +17,19 @@ export default function TenCentTeacake({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <ToastProvider>
-      <UserProvider>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <Layout>
-              <Component className="h-full" {...pageProps} />
-              <ToastContainer />
-            </Layout>
-          </Hydrate>
-        </QueryClientProvider>
-      </UserProvider>
-    </ToastProvider>
+    <Sentry.ErrorBoundary fallback={ErrorComponent}>
+      <ToastProvider>
+        <UserProvider>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <Layout>
+                <Component className="h-full" {...pageProps} />
+                <ToastContainer />
+              </Layout>
+            </Hydrate>
+          </QueryClientProvider>
+        </UserProvider>
+      </ToastProvider>
+    </Sentry.ErrorBoundary>
   );
 }
