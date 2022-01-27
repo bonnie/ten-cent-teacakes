9ccfdc071cab9d393b52d7c80c0a5ad5402ddb2c
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-26 16:42:57 -0800

    Try updating postcss to try to get tailwindcss classes in vercel deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+// https://github.com/vercel/next.js/discussions/17678#discussioncomment-92153
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 };
