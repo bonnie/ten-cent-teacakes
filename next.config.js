7d3097f9b31352cc04a9ae943b523a7fab006f38
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-27 15:27:26 -0800

    Restore sentry and disable error boundary

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
   // from https://stackoverflow.com/a/68098547
   webpack5: true,
   webpack: (config) => {
@@ -37,54 +39,18 @@ module.exports = {
   },
 };
 
-// const { withSentryConfig } = require("@sentry/nextjs");
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-// const moduleExports = {
-//   // from https://stackoverflow.com/a/68098547
-//   webpack5: true,
-//   webpack: (config) => {
-//     // eslint-disable-next-line no-param-reassign
-//     config.resolve.fallback = {
-//       fs: false,
-//       util: false,
-//       assert: false,
-//       stream: false,
-//       constants: false,
-//       path: false,
-//       module: false,
-//       process: false,
-//       crypto: require.resolve("crypto-browserify"),
-//     };
-//     return config;
-//   },
-//   images: {
-//     disableStaticImages: true,
-//   },
-//   env: {
-//     SENTRY_DSN: process.env.SENTRY_DSN,
-//     NODE: process.env.NODE,
-//     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-//     SESSION_COOKIE_LIFETIME: process.env.SESSION_COOKIE_LIFETIME,
-//     AUTH0_SECRET: process.env.AUTH0_SECRET,
-//     AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
-//     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-//     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-//     AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
-//   },
-// };
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  include: ".",
+  ignore: ["node_modules", "webpack.config.js"],
+};
 
-// const sentryWebpackPluginOptions = {
-//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//   // the following options are set automatically, and overriding them is not
-//   // recommended:
-//   //   release, url, org, project, authToken, configFile, stripPrefix,
-//   //   urlPrefix, include, ignore
-
-//   authToken: process.env.SENTRY_AUTH_TOKEN,
-//   include: ".",
-//   ignore: ["node_modules", "webpack.config.js"],
-// };
-
-// // Make sure adding Sentry options is the last code to run before exporting, to
-// // ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
