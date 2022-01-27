d3220c3ab133fee05e71b1ab065f98a0c892afb4
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-27 13:32:05 -0800

    Remove debugging

--- src/lib/auth/useWhitelistUser.ts
+++ src/lib/auth/useWhitelistUser.ts
@@ -12,12 +12,9 @@ type WhitelistUser = UserProfile | undefined;
 type WhitelistUserReturn = { user: UserProfile | undefined };
 
 export const useWhitelistUser = (): WhitelistUserReturn => {
-  const auth0 = useUser();
+  const { user: auth0User } = useUser();
   const [user, setUser] = useState<WhitelistUser>();
   const { showToast } = useToast();
-
-  const auth0User = auth0.user;
-  console.log("*******AUTH0", auth0);
 
   useQuery(
     queryKeys.whitelist,
