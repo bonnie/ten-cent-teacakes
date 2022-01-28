2c96ef7b4dc01c361d17bfe44449585ea9459014
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-01-27 22:33:10 -0800

    Remove baseURL for dev purposes

--- src/lib/axios/axiosInstance.ts
+++ src/lib/axios/axiosInstance.ts
@@ -4,11 +4,9 @@ const config: AxiosRequestConfig = {};
 
 if (process.env.NODE === "production") {
   config.baseURL = "https://tencentteacakes.com/";
-} else {
-  config.baseURL = process?.env?.VERCEL_URL || "http://localhost:3000/";
+} else if (process.env.NODE === "test ") {
+  config.baseURL = "http://localhost:3000/";
 }
-
-console.log("PROCESS.ENV", process.env);
 
 // for canceling requests to avoid test errors
 export const cancelTokenSource = axios.CancelToken.source();
