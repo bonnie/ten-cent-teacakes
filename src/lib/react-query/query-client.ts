851195ef18210e0ec1d7ec90dd5a6785e9c963e9
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-02-04 10:41:49 -0800

    Remove defaults that were messing with auth0 user and shows

--- src/lib/react-query/query-client.ts
+++ src/lib/react-query/query-client.ts
@@ -2,9 +2,6 @@ import { QueryClient } from "react-query";
 
 export const defaultQueryClientOptions = {
   queries: {
-    staleTime: 600000, // 10 minutes
-    cacheTime: 900000, // default cacheTime is 5 minutes; doesn't make sense for staleTime to exceed cacheTime
-    refetchOnMount: false,
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
   },
