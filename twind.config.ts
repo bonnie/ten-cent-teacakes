d7cff9317269227ac51c40d6752517070c9ecebd
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-02-14 14:52:33 -0800

    Restore former twind config

--- twind.config.ts
+++ twind.config.ts
@@ -1,20 +1,25 @@
-import twind, { strict } from "twind";
+import twind from "twind";
 
 const themeColors = require("./src/lib/colors");
+const colors = require("tailwindcss/colors");
 const forms: twind.Plugin = require("@twind/forms");
 
 const config = {
-  mode: strict,
   theme: {
-    extend: {
-      colors: {
-        aqua: themeColors.aqua,
-        transparent: "transparent",
-      },
-      fontFamily: {
-        display: "Delfina",
-        body: "Ubuntu",
-      },
+    colors: {
+      aqua: themeColors.aqua,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      green: colors.green,
+      red: colors.red,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      transparent: "transparent",
+    },
+    fontFamily: {
+      display: "Delfina",
+      body: "Ubuntu",
     },
   },
   plugins: {
