85b74584747de0ad21d72b7d311edc887a76508d
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-02-14 10:36:38 -0800

    Add [id] get tests

--- src/__tests__/api/tests/[id].get.test.ts
+++ src/__tests__/api/tests/[id].get.test.ts
@@ -1,31 +1,62 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
-import showHandler from "@/pages/api/shows/[id]";
-import venueHandler from "@/pages/api/venues/[id]";
+import { PhotoWithShowAndVenue } from "@/lib/photos/types";
+import { getPhotos } from "@/lib/prisma/queries/photos";
+import photoHandler from "@/pages/api/photos/[id]";
+
+// ------------------------------------------------------------------ //
+// EXPECTED GET DATA
+//
+// ids don't reset after deleteMany, so actual number is not predictable depending
+// on which tests ran before this one.
+const generatePhotoData = async (): Promise<PhotoWithShowAndVenue> => {
+  const photos = await getPhotos();
+  return photos[0];
+};
 
 // ------------------------------------------------------------------ //
 // TEST DATA
+// parametrizing in case there are other routes in the future with [id] GET
 const testData = [
   {
-    item: "venue",
-    handler: venueHandler,
-  },
-  {
-    item: "show",
-    handler: showHandler,
+    item: "photo",
+    handler: photoHandler,
+    generateData: generatePhotoData,
   },
 ];
 
 // ------------------------------------------------------------------ //
 // TEST FUNCTION
-test.each(testData)("test retrieving $item by id", async ({ handler }) => {
-  // await testApiHandler({
-  //   handler,
-  //   test: async ({ fetch }) => {
-  //     const res = await fetch({
-  //       method: "GET",
-  //     });
-  //     const json = await res.json();
-  //   },
-  // });
-});
+test.each(testData)(
+  "test retrieving $item by id",
+  async ({ handler, generateData }) => {
+    const item = await generateData();
+
+    await testApiHandler({
+      handler,
+      paramsPatcher: (params) => {
+        // eslint-disable-next-line no-param-reassign
+        params.id = item.id;
+      },
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: "GET",
+        });
+        const json = await res.json();
+
+        // account for Date / string type discrepancy from Prisma -> json
+        // This feels hack-y and I don't love it :-(
+        Object.getOwnPropertyNames(item).forEach((property) => {
+          try {
+            const date = new Date(json[property]);
+            if (json[property] === date.toISOString()) json[property] = date;
+            // eslint-disable-next-line no-empty
+          } finally {
+          }
+        });
+
+        expect(json).toEqual(item);
+      },
+    });
+  },
+);
