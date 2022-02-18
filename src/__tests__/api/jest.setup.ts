16c61f12d3bedf298e867efc268db51f9ae0ce7d
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-02-17 16:03:47 -0800

    Move prisma into api tests dir

--- src/__tests__/api/jest.setup.ts
+++ src/__tests__/api/jest.setup.ts
@@ -10,7 +10,7 @@ import "@testing-library/jest-dom/extend-expect";
 // eslint-disable-next-line no-unused-vars
 import { describe, expect, test } from "@jest/globals";
 
-import { resetDB } from '../prisma/reset-db'
+import { resetDB } from './prisma/reset-db'
 
 beforeAll(async () => {
   // seed db
