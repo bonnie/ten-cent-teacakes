53a87c21b5278bff8c350a3f2a4571d295c404b8
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-02-21 17:40:04 -0800

    Add 'shows' navigation test

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -12,7 +12,21 @@ describe("Navigation", () => {
     // The new url should include "/more"
     cy.url().should("include", "/more");
 
-    // The new page should contain an h1 with "more page"
+    // The new page should contain an h1 with "more"
     cy.get("h1").contains("More");
+  });
+  it("should navigate to the shows page", () => {
+    // Start from the index page
+    cy.visit("/");
+
+    // Find an img with alt text containing "shows" and click it
+    cy.findByAltText(/show/).click();
+
+    // The new url should include "/shows"
+    cy.url().should("include", "/shows");
+
+    // The new page should contain an h1 with "shows"
+    cy.get("h1").contains("Shows");
+    // cy.get("h1").contains("Past Shows");
   });
 });
