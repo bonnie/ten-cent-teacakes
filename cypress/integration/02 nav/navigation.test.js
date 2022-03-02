fa1933bb1f788ef2acd42ea693a42699dc3105f7
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-03-02 13:37:43 -0800

    Complete simple nav tests

--- cypress/integration/02 nav/navigation.test.js
+++ cypress/integration/02 nav/navigation.test.js
@@ -2,6 +2,9 @@ describe("Navigation", () => {
   beforeEach(() => {
     cy.intercept("/api/photos", { statusCode: 200, body: [] });
     cy.intercept("/api/shows", { statusCode: 200, body: [] });
+    cy.intercept("/api/musicians", { statusCode: 200, body: [] });
+    cy.intercept("/api/instruments", { statusCode: 200, body: [] });
+    cy.intercept("/api/venues", { statusCode: 200, body: [] });
   });
   it("should navigate to the more page", () => {
     // Start from the index page
@@ -14,7 +17,7 @@ describe("Navigation", () => {
     cy.url().should("include", "/more");
 
     // The new page should contain an h1 with "more"
-    cy.get("h1").contains("More");
+    cy.findByRole("heading", { name: "More" }).should("exist");
   });
   it("should navigate to the shows page", () => {
     // Start from the index page
@@ -27,7 +30,32 @@ describe("Navigation", () => {
     cy.url().should("include", "/shows");
 
     // The new page should contain an h1 with "shows"
-    cy.get("h1").contains("Shows");
-    // cy.get("h1").contains("Past Shows");
+    cy.findByRole("heading", { name: "Shows" }).should("exist");
+  });
+  it("should navigate to the band page", () => {
+    // Start from the index page
+    cy.visit("/");
+
+    // Find an img with alt text containing "band" and click it
+    cy.findByAltText(/band/).click();
+
+    // The new url should include "/band"
+    cy.url().should("include", "/band");
+
+    // The new page should contain an h1 with "band"
+    cy.findByRole("heading", { name: "The Band" }).should("exist");
+  });
+  it("should navigate to the photos page", () => {
+    // Start from the index page
+    cy.visit("/");
+
+    // Find an img with alt text containing "photos" and click it
+    cy.findByAltText(/photos/).click();
+
+    // The new url should include "/photos"
+    cy.url().should("include", "/photos");
+
+    // The new page should contain an h1 with "photos"
+    cy.findByRole("heading", { name: "Photos" }).should("exist");
   });
 });
