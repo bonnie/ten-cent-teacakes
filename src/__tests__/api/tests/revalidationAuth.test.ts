e541f1789bdc93d0876a89d552b9d6bec0ee7926
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-07-28 17:43:23 -0700

    Update revalidation tests to exclude shows

--- src/__tests__/api/tests/revalidationAuth.test.ts
+++ src/__tests__/api/tests/revalidationAuth.test.ts
@@ -1,11 +1,10 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
-import showIdHandler from "@/pages/api/shows/[id]";
-import showsHandler from "@/pages/api/shows/index";
+import photosHandler from "@/pages/api/photos/index";
 
-test("PUT /api/shows returns 401 status for incorrect revalidation secret", async () => {
+test("PUT /api/photos returns 401 status for incorrect revalidation secret", async () => {
   await testApiHandler({
-    handler: showsHandler,
+    handler: photosHandler,
     paramsPatcher: (params) => {
       params.queryStringURLParams = { secret: "NOT THE REAL SECRET" };
     },
@@ -16,35 +15,9 @@ test("PUT /api/shows returns 401 status for incorrect revalidation secret", asyn
   });
 });
 
-test("PATCH /api/shows/:id returns 401 status for incorrect revalidation secret", async () => {
+test("GET /api/photos DOES NOT return 401 status for missing revalidation secret", async () => {
   await testApiHandler({
-    handler: showIdHandler,
-    paramsPatcher: (params) => {
-      params.queryStringURLParams = { secret: "NOT THE REAL SECRET" };
-    },
-    test: async ({ fetch }) => {
-      const res = await fetch({ method: "PATCH" });
-      expect(res.status).toEqual(401);
-    },
-  });
-});
-
-test("DELETE /api/shows/:id returns 401 status for incorrect revalidation secret", async () => {
-  await testApiHandler({
-    handler: showIdHandler,
-    paramsPatcher: (params) => {
-      params.queryStringURLParams = { secret: "NOT THE REAL SECRET" };
-    },
-    test: async ({ fetch }) => {
-      const res = await fetch({ method: "DELETE" });
-      expect(res.status).toEqual(401);
-    },
-  });
-});
-
-test("GET /api/shows/:id DOES NOT return 401 status for missing revalidation secret", async () => {
-  await testApiHandler({
-    handler: showsHandler,
+    handler: photosHandler,
     test: async ({ fetch }) => {
       const res = await fetch({ method: "GET" });
       expect(res.status).toEqual(200);
